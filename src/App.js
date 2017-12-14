f3b341f59e3d981a6c73ad71e0dd854ba9a106b5
Audun Øhra <audunohera@gmail.com>
2017-12-15 00:37:41 +0100

    Oooooo
    COmmittt

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let defStyle = {color: '#fff', 'font-size':'4em', 'font-family': 'Roboto', padding: '5px'};
-let bgColor = '#010101';
-let title = 'Title.';
+let headerStyle = {color: '#fff', 'fontSize':'4em', 'fontFamily':'Open Sans', 'fontWeight':'700', padding: '5px', margin:'0px'};
+let titleStyle = {color: '#fff', 'fontSize':'1.5em', 'fontFamily':'Open Sans', 'fontWeight':'700', padding: '0px', margin:'0px 0px 10px 0px'};
+let underTextStyle = {color: '#fff', 'fontSize':'0.9em', 'fontFamily':'Open Sans', 'fontWeight':'400', padding: '0px', margin:'0px'};
+let title = 'TITLE.';
+let articles = {'borderStyle':'Solid', 'borderWidth': '2px', 'borderColor':'#fff', width: '21vw', display: 'inline-block', padding: '1vw'};
 
 class App extends Component {
   render() {
     return (
       <div id='container'>
         <div id='header'>
-          <h1 style={defStyle}>{title}</h1>
+          <h1 style={headerStyle}>{title}</h1>
         </div>
         <div id='main'>
+          <Example />
+          <Example />
+          <Example />
           <Example />
         </div>
       </div>
@@ -22,7 +26,12 @@ class App extends Component {
 }
 class Example extends Component {
   render() {
-      return(<h3 style={defStyle}>This is a simple component.</h3>);
+      return(
+      <div style={articles}>
+        <h3 style={titleStyle}>This is an Article Title.</h3>
+        <h4 style={underTextStyle}>This is text from an article. Lots and lots of text. It is very important to have a lot of text here so that it fills the page nicely.</h4>
+      </div>
+      );
   }
 }
 
